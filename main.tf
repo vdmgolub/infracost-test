cd51ba484224f75a314b63da0b8183ae224c4645
Vadim Golub <git@vdmgolub.com>
2021-11-21 14:56:20 +0100

    test action

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+  instance_type = "m5.8xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
   root_block_device {
     volume_size = 70
@@ -27,5 +27,5 @@ resource "aws_lambda_function" "hello_world" {
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
-  memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+  memory_size   = 512                      # <<<<< Try changing this to 512 to compare costs
 }
